6b5706134664a4de95a0468ae4d9214096cd1f9c
Arne Brasseur <arne@arnebrasseur.net>
2020-01-25 14:19:14 +0100

    Make sure we can use jediterm in dev and tests

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
-((nil . ((cider-clojure-cli-global-options . "-A:dev:test")))
+((nil . ((cider-clojure-cli-global-options . "-A:dev:test:jediterm")))
  (clojure . ((eval . (define-clojure-indent (for-all 1))))))
